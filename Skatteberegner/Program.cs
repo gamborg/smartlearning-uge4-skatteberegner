a8580497602fac1dc1bb4236a2f6ac8f96aceed3
Anders Gamborg <anders@agamborg.dk>
2024-09-05 09:58:42 +0200

    Added business logic to tax calculation

--- Skatteberegner/Program.cs
+++ Skatteberegner/Program.cs
@@ -1,10 +1,29 @@
-﻿    public class Beregning {
-        static void Main(string[] args) {
+﻿public class Beregning
+{
+    static void Main(string[] args)
+    {
 
-        }
-        
-        // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
-        public double SkatVedJulegave(double julegave, double andenGave) {
+    }
+
+    // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
+    public double SkatVedJulegave(double julegave, double andenGave)
+    {
+        var total = julegave + andenGave;
+        if (total <= 1200 || julegave <= 900 && andenGave <= 300)
+        {
             return 0;
         }
+        else if (julegave > 900 && andenGave <= 300)
+        {
+            return julegave + andenGave;
+        }
+        else if (julegave <= 900 && andenGave > 300)
+        {
+            return andenGave;
+        }
+        else
+        {
+            return julegave + andenGave;
+        }
     }
+}
